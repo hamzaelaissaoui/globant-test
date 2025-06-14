cc760cacc68ac9c9951f8227799ae853a3fa7f60
hamza elaissaoui <hamza.elaissaoui099@gmail.com>
2025-06-14 18:36:48 +0100

    feat: unsbcribe from fetching exams

--- frontend/src/app/components/exams-list/exams-list.component.ts
+++ frontend/src/app/components/exams-list/exams-list.component.ts
@@ -1,35 +1,38 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { Component, DestroyRef, OnInit, signal } from '@angular/core';
 
 import { Exam } from '../../models/exam.model';
 import { ExamsApiService } from '../../services/exams-api.service';
 import { AddExamFormComponent } from '../add-exam-form/add-exam-form.component';
 import { ExamHeaderComponent } from '../exam-header/exam-header.component';
 import { ExamItemComponent } from '../exam-item/exam-item.component';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-exams-list',
-  imports: [
-    ExamHeaderComponent,
-    ExamItemComponent,
-    AddExamFormComponent,
-  ],
+  imports: [ExamHeaderComponent, ExamItemComponent, AddExamFormComponent],
   templateUrl: './exams-list.component.html',
 })
 export class ExamsListComponent implements OnInit {
   public exams = signal<Exam[]>([]);
   public showForm = signal(false);
 
-  public constructor(private readonly examsApiService: ExamsApiService) {}
+  public constructor(
+    private readonly examsApiService: ExamsApiService,
+    private readonly destroyRef: DestroyRef
+  ) {}
 
   public ngOnInit(): void {
-    this.examsApiService.getExams().subscribe({
-      next: (exams) => {
-        this.exams.set(exams);
-      },
-      error: (err) => {
-        console.error('Error fetching exams:', err);
-      },
-    });
+    this.examsApiService
+      .getExams()
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe({
+        next: (exams) => {
+          this.exams.set(exams);
+        },
+        error: (err) => {
+          console.error('Error fetching exams:', err);
+        },
+      });
   }
 
   public addNewItem(): void {
